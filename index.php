9e9c2b787da62f7e47ea1d946eee3e4a5fc73337
Stuart Duncan <github@tycamtech.com>
2012-09-12 16:27:00 -0400

    Minor changes

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
 
-# DIR paths to primary components of this Framework.
 define('BASE_PATH', dirname(__FILE__));
 define('APP_PATH', BASE_PATH . DS . 'app' . DS);
 define('CORE_PATH', BASE_PATH . DS . 'core' . DS);
 
-// The core of the whole thing
 require_once CORE_PATH . 'Core.php';
-
-// Create the object and begin routing
 $core = new Core();
